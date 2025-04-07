eae375da2b8ce5d221cf3ab7c3977a4c1e6b12a0
Lulyatt <lulyatt@hotmail.co.uk>
2025-04-07 11:26:54 -0700

    Added codewars solution

--- 8kyu/Counting Sheep.js
+++ 8kyu/Counting Sheep.js
@@ -1,4 +1,3 @@
-// Description:
 // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 // For example,
@@ -11,9 +10,7 @@
 //   false, false, true,  true]
 // The correct answer would be 17.
 
-// Hint: Don't forget to check for bad values like null/undefined
-
-// My code:
+// My code 
 
 function countSheeps(sheep) {
     return sheep.filter(Boolean).length
--- 8kyu/Sum Arrays.js
+++ 8kyu/Sum Arrays.js
@@ -1,4 +1,3 @@
-// Description:
 // Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 
 // Examples
@@ -11,18 +10,12 @@
 // Input: [-2.398]
 // Output: -2.398
 
-// Assumptions
-// You can assume that you are only given numbers.
-// You cannot assume the size of the array.
-// You can assume that you do get an array and if the array is empty, return 0.
 
 // My code
-
-
 
 function sum (numbers) {
     let i = 0
     sumofNumbs = numbers.reduce((a, c) => a + c, i)
     
     return sumofNumbs
-  }
+}
