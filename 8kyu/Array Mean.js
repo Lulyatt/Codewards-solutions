d6582acc55e0fb4e66dac6c27ddb5771e4da8fba
Lulyatt <lulyatt@hotmail.co.uk>
2025-07-25 10:01:27 -0700

    Added coding challenge for 25-07-2025

---
+++ 8kyu/Array Mean.js
@@ -0,0 +1,17 @@
+// Find Mean
+// Find the mean (average) of a list of numbers in an array.
+
+// Information
+// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+// For an example list of 1, 3, 5, 7
+
+// 1. Add all of the numbers
+
+// 1+3+5+7 = 16
+
+// My code:
+
+function findAverage(nums) {
+  return nums.reduce((a, c) => a + c, 0) / nums.length
+}
