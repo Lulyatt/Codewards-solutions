fb7d74dc7aab5007d2ecebec5f9b3c5e8f37218e
Lulyatt <lulyatt@hotmail.co.uk>
2025-08-09 18:26:36 -0700

    Added coding challenge for 09-08-2025

---
+++ 8kyu/Get Planet Name By ID.js
@@ -0,0 +1,8 @@
+// The function is not returning the correct values. Can you figure out why?
+
+// My code:
+
+function getPlanetName(id) {
+  const planets = [null, 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  return planets[id];
+}
