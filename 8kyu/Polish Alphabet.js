14a61f06db0c483ddea39a5c204f15063ffce973
Lulyatt <lulyatt@hotmail.co.uk>
2025-09-18 16:37:33 +0100

    Added coding challenge for 18-09-2025

---
+++ 8kyu/Polish Alphabet.js
@@ -0,0 +1,22 @@
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// My code:
+
+function correctPolishLetters(string) {
+  return string.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/ł/g, 'l').replace(/Ł/g, 'L');         
+}
+
+
