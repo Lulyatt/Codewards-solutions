94faaa407c40fe4e78a6c32cdd2c025600008795
Luke Ulyatt <lulyatt@hotmail.co.uk>
2026-08-06 11:23:36 +0100

    Added coding challenge for 06-08-2026

--- 8kyu refresh/Century from year.js
+++ 8kyu refresh/Century from year.js
@@ -1,21 +1,21 @@
-Description:
+// Description:
 
-Introduction
-The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
 
-Task
-Given a year, return the century it is in.
+// Task
+// Given a year, return the century it is in.
 
-Examples
-1705 --> 18
-1900 --> 19
-1601 --> 17
-2000 --> 20
-2742 --> 28
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// 2742 --> 28
 
-My code:
+// My code:
 
-// function century(year) {
+function century(year) {
  
-//   return Math.ceil(year/100)
-// }
+  return Math.ceil(year/100)
+}
