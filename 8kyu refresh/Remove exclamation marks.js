342d6f5db11a04e71082ff4c5b7441560d952ded
Luke Ulyatt <lulyatt@hotmail.co.uk>
2026-08-10 12:22:22 +0100

    Added codewars solution.

---
+++ 8kyu refresh/Remove exclamation marks.js
@@ -0,0 +1,8 @@
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// My code:
+
+function remove(string) {
+  return string.endsWith('!') ? string.slice(0, -1) : string;
+}
